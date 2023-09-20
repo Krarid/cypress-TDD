6f1d00dc0db521b200a8d5bdb4559e9a6a92732c
Javier Meléndez <jmz.krarid@outlook.com>
2023-09-20 12:28:52 -0600

    Add HRM-2 test case
    The fixture file is being used for loading data and a customized command for login has been added

--- cypress/integration/login/Test.js
+++ cypress/integration/login/Test.js
@@ -1,9 +1,23 @@
+/// <reference types="Cypress" />
+
 describe('Login test cases', () => {
     it('Login with correct credentials', () => {
         cy.visit('https://opensource-demo.orangehrmlive.com/');
-        cy.get('input[name="username"]').type('Admin');
-        cy.get('input[name="password"]').type('admin123');
-        cy.get('button[class*="login"]').click();
+
+        cy.fixture('data').then( (data) => {
+            cy.login(data.username, data.password);
+        } )
+        
         cy.get('h6.oxd-text').should('have.text', 'Dashboard');
+    })
+
+    it('Login with incorrect credentials', () => {
+        cy.visit('https://opensource-demo.orangehrmlive.com/');
+
+        cy.fixture('data').then( (data) => {
+            cy.login(data.incorrectUsername, data.incorrectPassword);
+        } )
+
+        cy.get('div > p.oxd-alert-content-text').should('have.text', 'Invalid credentials');
     })
 })
